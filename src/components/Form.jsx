fde82bdf6cce8cf852788f242c4d47065c299b5a
ramnoa <ramzeynoah11@gmail.com>
2026-06-21 10:25:47 +0300

    input field with multiple values

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
 export default function Form() {
-    const [Name, setName] = useState("");
-    
+    //since we are using form we need to use state to store the value of the input field
+            // const [Name, setName] = useState("");
+     const [Name, setName] = useState({ FirstName: "", LastName: "" });       
     return (
         <div> 
             <form >
-                <input onChange={(e) => setName(e.target.value)}  type="text" value={Name}></input></form>  
+                <input onChange={(e) => setName({...Name, FirstName: e.target.value})}  type="text" value={Name.FirstName}></input>
+                <input onChange={(e) => setName({...Name, LastName: e.target.value})}  type="text" value={Name.LastName}></input>
+                </form>  
         </div>
     )
 }
