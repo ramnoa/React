12718811150f8d82ba3f5c5050f530dbf023047f
ramnoa <ramzeynoah11@gmail.com>
2026-06-22 09:58:29 +0300

    Handling form submission

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,12 +2,19 @@ import { useState } from "react";
 export default function Form() {
     //since we are using form we need to use state to store the value of the input field
             // const [Name, setName] = useState("");
-     const [Name, setName] = useState({ FirstName: "", LastName: "" });       
+     const [Name, setName] = useState({ FirstName: "", LastName: "" });
+     function handleSubmit(e) {
+        e.preventDefault();
+        //This where  the db will be connected but meanwhile we will just console log the name
+        console.log(Name);
+     }
+
     return (
         <div> 
             <form >
                 <input onChange={(e) => setName({...Name, FirstName: e.target.value})}  type="text" value={Name.FirstName}></input>
                 <input onChange={(e) => setName({...Name, LastName: e.target.value})}  type="text" value={Name.LastName}></input>
+                <button onClick={handleSubmit}>Submit</button>
                 </form>  
         </div>
     )
